e418569afeaba98252da99d4e59c8ba4d3e5c9bd
caihaha <1030878894@qq.com>
2021-04-14 23:30:25 +0800

    结构化消息

--- Socket/HelloSocket/EasyTcpClient/Client.cpp
+++ Socket/HelloSocket/EasyTcpClient/Client.cpp
@@ -12,6 +12,8 @@ enum CMD
 {
 	CMD_LOGIN,
 	CMD_LOGOUT,
+	CMD_LOGIN_RESULT,
+	CMD_LOGOUT_RESULT,
 };
 
 struct DataHeader
@@ -20,24 +22,46 @@ struct DataHeader
 	short dataLength;
 };
 // DataPacket
-struct Login
+struct Login : public DataHeader
 {
+	Login()
+	{
+		dataLength = sizeof(Login);
+		cmd = CMD_LOGIN;
+	}
 	char userName[32];
 	char PassWord[32];
 };
 
-struct LoginResult
+struct LoginResult : public DataHeader
 {
+	LoginResult()
+	{
+		dataLength = sizeof(LoginResult);
+		cmd = CMD_LOGIN_RESULT;
+		result = 0;
+	}
 	int result;
 };
 
-struct Logout
+struct Logout : public DataHeader
 {
+	Logout()
+	{
+		dataLength = sizeof(Logout);
+		cmd = CMD_LOGOUT;
+	}
 	char userName[32];
 };
 
-struct LogoutResult
+struct LogoutResult : public DataHeader
 {
+	LogoutResult()
+	{
+		dataLength = sizeof(LogoutResult);
+		cmd = CMD_LOGOUT_RESULT;
+		result = 0;
+	}
 	int result;
 };
 
@@ -80,29 +104,24 @@ int main()
 		}
 		else if (0 == strcmp(cmdBuf,"login"))
 		{
-			DataHeader dh = { CMD_LOGIN, sizeof(Login) };
-			send(_sock, (char*)&dh, sizeof(DataHeader), 0);
-			Login login = { "CJC", "123456" };
+			Login login;
+			strcpy_s(login.userName, "CJC");
+			strcpy_s(login.PassWord, "123456");
 			send(_sock, (char *)&login, sizeof(Login), 0);
 
 			// 接受数据
-			DataHeader retHeader = {};
 			LoginResult retLogin = {};
-			recv(_sock, (char *)&retHeader, sizeof(DataHeader), 0);
-			recv(_sock, (char*)&retHeader, sizeof(LoginResult), 0);
+			recv(_sock, (char*)&retLogin, sizeof(LoginResult), 0);
 			printf("LoginResult %d\n", retLogin.result);
 		}
 		else if (0 == strcmp(cmdBuf, "logout"))
 		{
-			Logout logout = { "CJC"};
-			DataHeader dh = { CMD_LOGOUT, sizeof(Logout) };
-			send(_sock, (char *)&dh, sizeof(DataHeader), 0);
-			send(_sock, (char *)&logout, sizeof(Login), 0);
+			Logout logout;
+			strcpy_s(logout.userName, "CJC");
+			send(_sock, (char *)&logout, sizeof(Logout), 0);
 
-			DataHeader retHeader = {};
 			LogoutResult retLogout = {};
-			recv(_sock, (char*)&retHeader, sizeof(DataHeader), 0);
-			recv(_sock, (char*)&retHeader, sizeof(LogoutResult), 0);
+			recv(_sock, (char*)&retLogout, sizeof(LogoutResult), 0);
 			printf("LogoutResult %d\n", retLogout.result);
 		}
 		else
--- Socket/HelloSocket/EasyTcpServer/Server.cpp
+++ Socket/HelloSocket/EasyTcpServer/Server.cpp
@@ -12,6 +12,8 @@ enum CMD
 {
 	CMD_LOGIN,
 	CMD_LOGOUT,
+	CMD_LOGIN_RESULT,
+	CMD_LOGOUT_RESULT,
 };
 
 struct DataHeader
@@ -20,24 +22,46 @@ struct DataHeader
 	short dataLength;
 };
 // DataPacket
-struct Login
+struct Login : public DataHeader
 {
+	Login()
+	{
+		dataLength = sizeof(Login);
+		cmd = CMD_LOGIN;
+	}
 	char userName[32];
 	char PassWord[32];
 };
 
-struct LoginResult
+struct LoginResult : public DataHeader
 {
+	LoginResult()
+	{
+		dataLength = sizeof(LoginResult);
+		cmd = CMD_LOGIN_RESULT;
+		result = 0;
+	}
 	int result;
 };
 
-struct Logout
+struct Logout : public DataHeader
 {
+	Logout()
+	{
+		dataLength = sizeof(Logout);
+		cmd = CMD_LOGOUT;
+	}
 	char userName[32];
 };
 
-struct LogoutResult
+struct LogoutResult : public DataHeader
 {
+	LogoutResult()
+	{
+		dataLength = sizeof(LogoutResult);
+		cmd = CMD_LOGOUT_RESULT;
+		result = 0;
+	}
 	int result;
 };
 
@@ -105,18 +129,16 @@ int main()
 		case CMD_LOGIN:
 		{
 			Login login = {};
-			recv(_cSock, (char*)&login, sizeof(Login), 0);
-			send(_cSock, (char*)&header, sizeof(DataHeader), 0);
-			LoginResult inRet = { 1 };
+			recv(_cSock, (char*)&login + sizeof(DataHeader), sizeof(Login) - sizeof(DataHeader), 0);
+			LoginResult inRet;
 			send(_cSock, (char*)&inRet, sizeof(LoginResult), 0);
 			break;
 		}
 		case CMD_LOGOUT:
 		{
 			Logout logout = {};
-			recv(_cSock, (char*)&logout, sizeof(Logout), 0);
-			send(_cSock, (char*)&header, sizeof(DataHeader), 0);
-			LogoutResult outRet = { 1 };
+			recv(_cSock, (char*)&logout + sizeof(DataHeader), sizeof(Logout) - sizeof(DataHeader), 0);
+			LogoutResult outRet;
 			send(_cSock, (char*)&outRet, sizeof(LogoutResult), 0);
 			break;
 		}
