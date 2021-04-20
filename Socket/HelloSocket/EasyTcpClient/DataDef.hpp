031075616d62eebf7d1abaea36f3e914b69f5256
caijinchao <caijinchao@xd.com>
2021-04-20 20:33:52 +0800

    客户端面向对象

---
+++ Socket/HelloSocket/EasyTcpClient/DataDef.hpp
@@ -0,0 +1,78 @@
+enum CMD
+{
+	CMD_LOGIN,
+	CMD_LOGIN_RESULT,
+	CMD_LOGOUT,
+	CMD_LOGOUT_RESULT,
+	CMD_NEW_USER_JOIN,
+	CMD_ERROR,
+};
+
+struct DataHeader
+{
+	short cmd;
+	short dataLength;
+};
+// DataPacket
+struct Login : public DataHeader
+{
+	Login()
+	{
+		dataLength = sizeof(Login);
+		cmd = CMD_LOGIN;
+	}
+	char userName[32];
+	char PassWord[32];
+};
+
+struct LoginResult : public DataHeader
+{
+	LoginResult()
+	{
+		dataLength = sizeof(LoginResult);
+		cmd = CMD_LOGIN_RESULT;
+		result = 0;
+	}
+	int result;
+};
+
+struct Logout : public DataHeader
+{
+	Logout()
+	{
+		dataLength = sizeof(Logout);
+		cmd = CMD_LOGOUT;
+	}
+	char userName[32];
+};
+
+struct LogoutResult : public DataHeader
+{
+	LogoutResult()
+	{
+		dataLength = sizeof(LogoutResult);
+		cmd = CMD_LOGOUT_RESULT;
+		result = 0;
+	}
+	int result;
+};
+
+struct Error : public DataHeader
+{
+	Error()
+	{
+		dataLength = sizeof(LogoutResult);
+		cmd = CMD_ERROR;
+	}
+};
+
+struct NewUserJoin : public DataHeader
+{
+	NewUserJoin()
+	{
+		dataLength = sizeof(NewUserJoin);
+		cmd = CMD_NEW_USER_JOIN;
+		result = 0;
+	}
+	int result;
+};
---
+++ Socket/HelloSocket/EasyTcpClient/EasyTcpClient.hpp
@@ -0,0 +1,221 @@
+#ifndef _EasyTcpClient_hpp_
+#define _EasyTcpClient_hpp_
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <WinSock2.h>
+#include <windows.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <unistd>
+#include <arpa/inet.h>
+#include <string.h>
+#define SOCKET int
+#define INVALID_SOCKET  (SOCKET)(0)
+#define SOCKET_ERROR (-1)
+#endif
+
+#include <stdio.h>
+#include <thread>
+
+#include "DataDef.hpp"
+
+class EasyTcpClient
+{
+public:
+	EasyTcpClient()
+	{
+
+	}
+	virtual ~EasyTcpClient()
+	{
+		_sock = INVALID_SOCKET;
+	}
+
+private:
+	// 建立socket
+	int InitSocket();
+
+	// 连接socket
+	int Connect(char* ip, unsigned short port);
+
+	// 关闭socket
+	void Close();
+
+	// 发送数据
+	int SendData(DataHeader* header);
+	void OnNetMsg(SOCKET _sock, DataHeader* header);
+
+	// 接受数据
+	int RecvData(SOCKET _sock);
+
+	// 处理网络消息
+	bool DealMsg();
+
+	bool IsRun() { return _sock != INVALID_SOCKET; }
+private:
+	SOCKET _sock;
+};
+
+int EasyTcpClient::InitSocket()
+{
+	if (_sock != INVALID_SOCKET)
+	{
+		printf("_sock exist, close and new socket\n");
+		Close();
+	}
+#ifdef _WIN32
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA data;
+	WSAStartup(ver, &data); // 启动socket
+#endif
+	// TODO 编写网络编程代码
+	_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (_sock == INVALID_SOCKET)
+	{
+		printf("socket creat errer\n");
+	}
+	else
+	{
+		printf("socket creat success\n");
+	}
+
+	return _sock;
+}
+
+int EasyTcpClient::Connect(char *ip, unsigned short port)
+{
+	if (_sock == INVALID_SOCKET)
+	{
+		InitSocket();
+	}
+
+	sockaddr_in _sin = {};
+	_sin.sin_family = AF_INET;
+	_sin.sin_port = htons(port);
+#ifdef _WIN32
+	_sin.sin_addr.S_un.S_addr = inet_addr(ip);
+#else
+	_sin.sin_addr.S_addr = inet_addr(ip);
+#endif // _WIN32
+	int ret = connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in));
+	if (ret == SOCKET_ERROR)
+	{
+		printf("connect error\n");
+	}
+	else
+	{
+		printf("connect success\n");
+	}
+	return ret;
+}
+
+void EasyTcpClient::Close()
+{
+	if (_sock == INVALID_SOCKET)
+	{
+		return;
+	}
+
+#ifdef _WIN32
+	closesocket(_sock);
+	WSACleanup(); // 和WSAStartup匹配
+#else
+	close(_sock);
+#endif
+}
+
+bool EasyTcpClient::DealMsg()
+{
+	if(!IsRun())
+	{
+		return false;
+	}
+	// 伯克利 socket
+	fd_set fdRead;
+	FD_ZERO(&fdRead);
+	FD_SET(_sock, &fdRead);
+
+	timeval t = { 1, 0 };
+	// 第一个参数nfds是一个整数值，指fd_set集合中所有描述符(socket)的范围(即最大socket+1)
+	// windows中不需要
+	int ret = select(_sock + 1, &fdRead, 0, 0, &t);
+	if (ret < 0)
+	{
+		printf("ret < 0, select exit\n");
+		return false;
+	}
+
+	if (FD_ISSET(_sock, &fdRead))
+	{
+		FD_CLR(_sock, &fdRead);
+		// 3 向服务器发送数据
+		if (RecvData(_sock) == -1)
+		{
+			printf("msg send == -1, select exit.\n");
+			return false;;
+		}
+	}
+
+	return true;
+}
+
+int EasyTcpClient::RecvData(SOCKET _cSock)
+{
+	// 3 接受服务端数据
+	char szRecv[4096] = {};
+	int recvLen = (int)recv(_cSock, szRecv, sizeof(DataHeader), 0);
+	DataHeader* header = (DataHeader*)szRecv;
+	if (recvLen <= 0)
+	{
+		printf("client exit\n");
+		return -1;
+	}
+
+	recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
+	OnNetMsg(_cSock, header);
+
+	return -1;
+}
+
+void EasyTcpClient::OnNetMsg(SOCKET _sock, DataHeader *header)
+{
+	switch (header->cmd)
+	{
+	case CMD_LOGIN_RESULT:
+	{
+		LoginResult* inRet = (LoginResult*)header;
+		printf("recv server data. length : %d, cmd : %d\n", inRet->dataLength, inRet->cmd);
+		break;
+	}
+	case CMD_LOGOUT_RESULT:
+	{
+		LogoutResult* outRet = (LogoutResult*)header;
+		printf("recv server data : length : %d, cmd : %d\n", outRet->dataLength, outRet->cmd);
+		break;
+	}
+	case CMD_NEW_USER_JOIN:
+	{
+		NewUserJoin* userJoin = (NewUserJoin*)header;
+		printf("recv server data : length : %d, cmd : %d\n", userJoin->dataLength, userJoin->cmd);
+		break;
+	}
+	default:
+		printf("error cmd.\n");
+	}
+
+}
+
+int EasyTcpClient::SendData(DataHeader* header)
+{
+	if (IsRun() && header != NULL)
+	{
+		return send(_sock, (const char*)&header, header->dataLength, 0);
+	}
+
+	return SOCKET_ERROR;
+}
+
+
+#endif // !_EasyTcpClient_hpp_
